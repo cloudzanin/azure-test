2a102458d208b23690e28e85d727b3291dfb449e
mehmetnuricetin <51626115+mehmetnuricetin@users.noreply.github.com>
2026-02-16 13:02:10 +0100

    windows

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 provider "azurerm" {
   features {
     resource_group {
@@ -7,76 +6,38 @@ provider "azurerm" {
   }
 }
 
-/*
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "tf-stage-azure-open-ai"
-    storage_account_name = "terraformstateopen2024ai"
-    container_name       = "terraformopenai2024"
-    key                  = "terraform.tfstate"
-  }
-}*/
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources5"
-  location = "Sweden Central"
-}
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network3"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "example-subnet3"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic3"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "internal2"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_virtual_machine" "example" {
-  name                  = "example-machine3"
+resource "azurerm_virtual_machine" "windows" {
+  name                  = "example-windows-machine"
   location              = azurerm_resource_group.example.location
   resource_group_name   = azurerm_resource_group.example.name
   network_interface_ids = [azurerm_network_interface.example.id]
   vm_size               = "Standard_B1s"
 
   storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
     version   = "latest"
   }
 
   storage_os_disk {
-    name              = "example-os-disk"
+    name              = "example-windows-os-disk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
 
   os_profile {
-    computer_name  = "example-machine"
+    computer_name  = "example-windows-machine"
     admin_username = "adminuser"
     admin_password = "AdminPassword123!"
   }
 
-  os_profile_linux_config {
-    disable_password_authentication = false
+  os_profile_windows_config {
+    provision_vm_agent        = true
+    enable_automatic_upgrades = true
   }
 
   tags = {
