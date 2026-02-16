fd5eeeedf2180a424325a15417ffe253e0c6a37f
mehmetnuricetin <51626115+mehmetnuricetin@users.noreply.github.com>
2026-02-16 11:26:14 +0100

    test

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ terraform {
 
 resource "azurerm_resource_group" "example" {
   name     = "example-resources5"
-  location = "West Europe"
+  location = "Sweden Central"
 }
 
 resource "azurerm_virtual_network" "example" {
@@ -53,7 +53,7 @@ resource "azurerm_virtual_machine" "example" {
   location              = azurerm_resource_group.example.location
   resource_group_name   = azurerm_resource_group.example.name
   network_interface_ids = [azurerm_network_interface.example.id]
-  vm_size               = "Standard_DS1_v2"
+  vm_size               = "Standard_B1s"
 
   storage_image_reference {
     publisher = "Canonical"
